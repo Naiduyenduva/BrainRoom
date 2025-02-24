bfbba7b4f14c5e492b1b502e35a22fe522f4d088
Laxmunaidu <luckyyenduva8.7@gmail.com>
2025-02-24 13:43:43 +0530

    added exam_attempt

---
+++ app/api/exams/attemptSubmit/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prismadb";
+export async function POST (req:NextRequest) {
+    try {
+        const { userId, testId, answers } = await req.json();
+
+        const questions = await prisma.mCQ.findMany ({ where: { testId },select: { id:true,correctOption:true }})
+
+        let score = 0;
+        questions.forEach((question) => {
+            const userAnswer = answers.find((ans:any) => ans.questionId === question.id)
+            if(userAnswer && userAnswer.selectedOption === question.correctOption) {
+                score++;
+            }
+        });
+
+        const updatedAttempt = await prisma.attempt.update({
+            where: {
+                userId_testId: { userId,testId }
+            },
+            data: {
+                score
+            },
+        });
+        return NextResponse.json({ message: "Test submitted",score,attempt:updatedAttempt },{ status: 200 })
+
+    } catch (error) {
+        return NextResponse.json({ message: "Internal server error", error },{ status: 500 });
+    }
+}
---
+++ app/api/exams/attemptsStart/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prismadb";
+export async function POST (req: NextRequest) {
+    try {
+        const { userId, testId } = await req.json();
+
+        const existingAttempt = await prisma.attempt.findUnique({
+            where: {
+                userId_testId: {userId,testId}
+            },
+        });
+        if(existingAttempt) {
+            return NextResponse.json({ message: "User already attempted the test" },{ status: 200 });
+        }
+        const totalQuestions = await prisma.mCQ.count({ where: { testId }});
+
+        const attempt = await prisma.attempt.create({
+            data: {
+                userId,
+                testId,
+                score: 0,
+                totalQuestions
+            }
+        })
+
+        return NextResponse.json({ message: "attempt started "} ,{ status: 201 })
+    } catch (error) {
+        return NextResponse.json ({ message: "Internal server error",error}, { status: 500 })
+    }
+}
